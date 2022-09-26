51265fa9df3ff448bbe4f2ba768e31131912c06e
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-09-26 21:10:17 +0100

    Only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = my_list[:]
+
+    for i in range(len(new_list)):
+        if new_list[i] % 2 == 0:
+            new_list[i] = True
+        else:
+            new_list[i] = False
+    return (new_list)
