c200d066df8b725d7782dcc8ce1c5b77741908fe
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-09-26 20:33:07 +0100

    Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for i in range(len(my_string)):
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            continue
+        new_string += my_string[i]
+    return new_string
