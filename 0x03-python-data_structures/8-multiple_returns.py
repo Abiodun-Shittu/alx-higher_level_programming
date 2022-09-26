5de788e97def67563fff07fe7b9a0e6525b13be0
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-09-26 20:53:37 +0100

    More returns!

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length == 0:
+        first_char = None
+        _tuple = (length, first_char)
+        return _tuple
+    else:
+        first_char = sentence[0]
+        _tuple = (length, first_char)
+        return _tuple
