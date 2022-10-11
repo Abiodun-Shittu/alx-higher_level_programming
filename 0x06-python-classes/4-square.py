294f2a6d1cbb673de6a549c271ba50ab5957495b
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-10-11 23:11:01 +0100

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size * self.__size)
