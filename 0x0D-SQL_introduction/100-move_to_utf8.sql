73707b44760caf23a81e81a0e0db0775d4362882
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-11-29 19:24:59 +0100

    Go to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- script that converts hbtn_0c_0 database to UTF8
+USE hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
