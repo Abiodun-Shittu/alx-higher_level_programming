652e613f03c8d6707ab2ccb35521df282afc2ec0
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-11-29 19:18:28 +0100

    Number by score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0
+SELECT score,
+	COUNT(score) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
