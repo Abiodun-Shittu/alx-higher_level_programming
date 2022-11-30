e0be6b9a563f41c83fa48ea00809c149f8788ecf
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-11-30 22:56:33 +0100

    List shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,8 @@
--- script that lists all shows contained in hbtn_0d_tvshows without a genre linked
+-- Script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
 SELECT tv_shows.title,
 	tv_genres.name
-FROM tv_genres
-	RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-	RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-ORDER by tv_shows.title;
+FROM tv_shows
+	LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+	LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title ASC,
+	tv_genres.name ASC;
