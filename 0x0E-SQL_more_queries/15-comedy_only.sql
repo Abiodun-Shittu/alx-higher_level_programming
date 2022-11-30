74dbbb90b3ee1438ce8c62cb559bfab54026734e
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-11-30 22:17:02 +0100

    Only Comedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- script that lists all shows contained in hbtn_0d_tvshows without a genre linked
+SELECT tv_shows.title
+FROM tv_shows
+	INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+	INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
