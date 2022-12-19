107cd6f81905b7b2154fbe360a697b2b322d755d
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-12-19 15:02:56 +0100

    An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const firstArg = parseInt(process.argv[2]);
+if (!firstArg || isNaN(firstArg)) {
+  console.log('Not a number');
+} else {
+  console.log('My Number: ' + firstArg);
+}
