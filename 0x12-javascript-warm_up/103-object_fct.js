cf392859cdacb3041092c5e991cbbd3a89a596c7
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-12-20 03:57:08 +0100

    Increment object

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -5,10 +5,9 @@ const myObject = {
 };
 console.log(myObject);
 
-function incr () {
+myObject.incr = function () {
   myObject.value += 1;
-}
-myObject.incr = incr;
+};
 
 myObject.incr();
 console.log(myObject);
