786f9238b6708af1da1bcb7f8136fd298de0811a
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-12-20 03:50:02 +0100

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,5 +3,5 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const arr = process.argv.slice(2).map(Number);
-  console.log(arr.sort()[arr.length - 2]);
+  console.log(arr.sort((a, b) => a - b)[arr.length - 2]);
 }
