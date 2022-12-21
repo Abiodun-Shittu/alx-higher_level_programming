5346dc3cfd11da3937dd3d1819a83da1f26c2064
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-12-21 14:18:48 +0100

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const parentSquare = require('./5-square');
+
+module.exports = class Square extends parentSquare {
+  charPrint (c) {
+    if (typeof (c) === 'undefined') {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
