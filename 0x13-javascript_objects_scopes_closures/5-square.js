bed94e0bda680703f49c47f251c23b49105b97ec
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-12-21 14:13:31 +0100

    Square #0

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle');
 
-module.exports = class square extends Rectangle {
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
