5b180bb45c66616a41021eb0fa4e8183d6e5930d
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-12-23 17:15:02 +0100

    Get a state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script that lists all objects that contain the a letter from the database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name.like(sys.argv[4]))
+    if states.count() != 1 or not states:
+        print("Not found")
+    else:
+        print("{}".format(states.first().id))
