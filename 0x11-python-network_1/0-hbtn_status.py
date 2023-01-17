75dc8a0ab42e80b11f459d108010fe2b49971c47
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-01-17 21:12:18 +0100

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A Python script that fetches https://alx-intranet.hbtn.io/status """
+
+
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
